6bca8048b3a9791e22eaf590fa9a0f8abea2c2b7
lu22111 <139249445+lu22111@users.noreply.github.com>
2024-10-10 17:57:04 -0600

    Actividad10/10/2024
    Actividad10/10/2024

--- introlaravel/resources/views/componentes.blade.php
+++ introlaravel/resources/views/componentes.blade.php
@@ -6,7 +6,16 @@
 <x-card encabezado="componente" titulo="Dinamico" textoBoton="Guardar">
 </x-card>
 soy el contenido del primero
+
 <x-card encabezado="componente2" titulo="Dinamico 2" textoBoton="No Guardar">
 </x-card>
 otro gato
+
+<x-Alert tipo="danger"> rojo</x-Alert>
+<x-Alert tipo="warning"> amarillo</x-Alert>
+
+
+
+
+
 @endsection
---
+++ introlaravel/resources/views/components/alert.blade.php
@@ -0,0 +1,3 @@
+<div class="alert alert-{{$tipo}}" role="alert">
+   soy un Alert {{$slot}}
+</div>

96a129b447ae472adf685d8519664681cffa0f87
lu22111 <139249445+lu22111@users.noreply.github.com>
2024-10-24 18:04:08 -0600

    practica6

--- introlaravel/app/Http/Controllers/ControladorVistas.php
+++ introlaravel/app/Http/Controllers/ControladorVistas.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Http\Requests\ValidorCliente;
 class ControladorVistas extends Controller
 {
     // Usamos este controlador para administrar la lógica de las vistas
@@ -22,8 +22,19 @@ class ControladorVistas extends Controller
         return view('clientes');
     }
 
-    public function procesadorCliente(Request $peticion)
+    public function procesadorCliente(ValidorCliente $peticion)
     {
+        $validacion = $peticion->validate([
+            'txtnombre' => 'required|min:4|max:20',
+            'txtapellido' => 'required',
+            'txtcorreo' => 'required',
+            'txttelefono' => 'required|numeric',
+        ]);
+
+        $usuarios = $peticion->input('txtnombre');
+        session()->flash('exito', 'Se guardó el usuario: ' . $usuarios);
+        return to_route('rutaForm');
+
         // //return ':)Si llegó la info del cliente :)';//
         // //return $peticion ->all();//
         // return $peticion ->ip();
@@ -32,10 +43,6 @@ class ControladorVistas extends Controller
         //return redirect()->route('rutaclientes');//
         //return back();//
         //$id=[['usuario'=>1],['usuario'=>2]];
-       // return view('formulario',compact('id'));
-$usuarios= $peticion->input('txtnombre');
-session()->flash('exito','se guardo el usuario:'.$usuarios);
-return to_route('rutaForm');
+        // return view('formulario', compact('id'));
     }
-}//
-
+}
--- introlaravel/resources/views/Formulario.blade.php
+++ introlaravel/resources/views/Formulario.blade.php
@@ -10,13 +10,13 @@
     <x-Alert tipo="warning">{{$value}}</x-Alert>
     @endsession
     @session('exito')
-    {!<script>
+    <script>
     Swal.fire({
-  title: 'Error!',
-  text: 'Do you want to continue',
-  icon: 'error',
+  title:'Respuesta servidor!',
+  text: '{{$value}}',
+  icon: 'success',
   confirmButtonText: 'Cool'
-})</script>!}
+})</script>
     @endsession
     <div class="card font-monospace">
         <div class="card-header fs-5 text-center text-primary">
@@ -30,19 +30,23 @@
 
                 <div class="mb-3">
                     <label for="nombre" class="form-label">Nombre:</label>
-                    <input type="text" class="form-control" name="txtnombre">
+                    <input type="text" class="form-control" name="txtnombre" value="{{old('txtnombre')}}">
+                    <small class="text-danger fst-italic">{{$errors->first('txtnombre') }}</small>
                 </div>
                 <div class="mb-3">
                     <label for="apellido" class="form-label">Apellido:</label>
-                    <input type="text" class="form-control" name="txtapellido">
+                    <input type="text" class="form-control" name="txtapellido" value="{{old('txtapellido')}}">
+                    <small class="text-danger fst-italic">{{$errors->first('txtapellido') }}</small>
                 </div>
                 <div class="mb-3">
                     <label for="correo" class="form-label">Correo:</label>
-                    <input type="email" class="form-control" name="txtcorreo">
+                    <input type="text" class="form-control" name="txtcorreo" value="{{old('txtcorreo')}}">
+                    <small class="text-danger fst-italic">{{$errors->first('txtcorreo') }}</small>
                 </div>
                 <div class="mb-3">
                     <label for="telefono" class="form-label">Teléfono:</label>
-                    <input type="number" class="form-control" name="txttelefono">
+                    <input type="text" class="form-control" name="txttelefono" value="{{old('txttelefono')}}">
+                    <small class="text-danger fst-italic">{{$errors->first('txttelefono') }}</small>
                 </div>
                 <div class="d-grid gap-2 mt-2 mb-1">
                     <button type="submit" class="btn btn-success btn-sm">Guardar Cliente</button>

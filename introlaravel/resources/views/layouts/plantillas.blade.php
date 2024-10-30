51b8c6e5719506e60d64d5f9e8c70d6a9d9e2ed7
lu22111 <139249445+lu22111@users.noreply.github.com>
2024-10-29 22:35:27 -0600

    6.1

--- introlaravel/resources/views/layouts/plantillas.blade.php
+++ introlaravel/resources/views/layouts/plantillas.blade.php
@@ -13,17 +13,17 @@
     <!--Inicia Navbar-->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a href="{{ route('rutainicio') }}" class="navbar-brand">Turista sin Hops</a>
+            <a href="{{ route('rutainicio') }}" class="navbar-brand">{{__('Turista sin Maps') }}</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a href="{{ route('rutaForm') }}" class="nav-link {{ request()->routeIs('rutaForm') ? 'text-warning' : '' }}" aria-current="page">Registro Clientes</a>
+                        <a href="{{ route('rutaForm') }}" class="nav-link {{ request()->routeIs('rutaForm') ? 'text-warning' : '' }}" aria-current="page">{{__('Registro Clientes')}}</a>
                     </li>
                     <li class="nav-item">
-                        <a href="{{ route('rutaclientes') }}" class="nav-link">Consulta Clientes</a>
+                        <a href="{{ route('rutaclientes') }}" class="nav-link">{{__('Turista sin Maps') }}</a>
                     </li>                </ul>
             </div>
         </div>

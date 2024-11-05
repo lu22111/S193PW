189c43685fe91bfe8b6324a933551a43cdbd8094
lu22111 <139249445+lu22111@users.noreply.github.com>
2024-11-05 17:32:40 -0600

    .

--- Repaso2/resources/views/principal.blade.php
+++ Repaso2/resources/views/principal.blade.php
@@ -7,7 +7,7 @@
         <div class="card-body">
             <h5 class="card-title">Mejoras libros del 2024</h5>
             <p class="card-text">
-                <img src="{{ asset('imagenes/biblioteca1.jpg') }}" class="card-img-top img-fluid" style="max-width: 50%;" alt="Imagen de la biblioteca">
+                <img src="{{ asset('imagenes/libro.jpg') }}" class="card-img-top img-fluid" style="max-width: 50%;" alt="Imagen de la biblioteca">
                 <br><br>
                 En 2024, la industria editorial ha dado un salto cuántico con la introducción de los "Libros Inteligentes". Estos innovadores libros no solo contienen texto, sino que también están equipados con inteligencia artificial que permite a los lectores interactuar con el contenido de maneras nunca antes vistas.
             </p>
